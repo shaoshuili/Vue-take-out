765e88420f6b551e6250bb98c35dd6befb78d38e
[shaoshuili] <[825141614@qq.com]>
2018-10-11 14:16:16 +0800

    login

--- src/main.js
+++ src/main.js
@@ -1,13 +1,21 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
+import router from "./router";
+import TopHeader from "./components/TopHeader/TopHeader.vue";
 
-Vue.config.productionTip = false
 
+//注册全局组件("组件名",组件)
+Vue.component("TopHeader",TopHeader);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  components: { App },
-  template: '<App/>'
+  /* components: { App },
+   template: '<App/>'
+   */
+  /*render: function (createElement) {  // 函数返回的标签会被渲染到el中
+    return createElement(App)   // '<App/>'
+  }*/
+  render: h => h(App),
+
+  router,
 })
